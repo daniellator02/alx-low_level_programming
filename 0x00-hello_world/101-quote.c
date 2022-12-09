8ba0903abcbd4f235048cf25c38c5beecebdb1e0
daniellator <daniellator02@gmail.com>
2022-12-09 04:45:37 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,3 +1,4 @@
+
 #include <stdio.h>
 #include <unistd.h>
 
@@ -5,9 +6,9 @@
  * main - prints exactly and that piece of art is useful 
  * 
  * Return: Always 0 (Success) 
- */ 
+*/
 int main(void)
 {	
- write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59); 
-  return (1); 
+       write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59); 
+       return (1); 
 }
