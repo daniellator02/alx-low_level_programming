6be85f1abfa4a76f5bbc6024c85801199730f0e3
daniellator <daniellator02@gmail.com>
2022-12-12 16:51:28 -0800

    2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -7,10 +7,12 @@
  */
 int main(void)
 {
+	char ch;
 
-	for (char ch = 'a'; ch <= 'z'; ch++)
+	for (ch = 'a'; ch <= 'z'; ch++)
 	{
 		putchar(ch);
 	}
 	putchar('\n');
-	return (0);					}
+	return (0);
+}
