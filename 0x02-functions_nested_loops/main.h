28bc60cf531b33ff6c6c5aa7bb844118ad67fd81
daniellator <daniellator02@gmail.com>
2022-12-14 19:33:51 -0800

    5-sign.c

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -2,10 +2,17 @@
 #define MAIN_H
 
 int _putchar(char c);
-void print_alphabet_x10(void);
 void print_alphabet(void);
 int _islower(int c);
-
+int print_sign(int n);
+int _isalpha(int c);
+void print_alphabet_x10(void);
+int _abs(int);
+int print_last_digit(int);
+void jack_bauer(void);
+void times_table(void);
+int add(int, int);
+void print_to_98(int n);
 
 
 
