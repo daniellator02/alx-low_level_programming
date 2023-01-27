d5f52848650ae300f294890da96f4e0ee30b4489
daniellator <daniellator02@gmail.com>
2023-01-27 15:17:39 -0800

    0x12-singly_linked_lists

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "lists.h"
+/**
+*print_list - print string and length of string for each element of 'h'
+*@h: pointer to first element of list
+*Return: length of list
+*/
+size_t print_list(const list_t *h)
+{
+	size_t size = 0;
+
+	while (h)
+	{
+		if (h->str == NULL)
+		{
+			printf("[%u] (nil)\n", h->len);
+			h = h->next;
+			++size;
+			continue;
+		}
+		printf("[%u] %s\n", h->len, h->str);
+		h = h->next;
+		++size;
+	}
+	return (size);
+}
---
+++ 0x12-singly_linked_lists/README.md
@@ -0,0 +1,1 @@
+Readme on 0x12-singly_linked_lists
